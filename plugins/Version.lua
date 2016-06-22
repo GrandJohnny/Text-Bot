a0b299bdc2a5bf9777da907014fd71e5eace07ed
Mя Jαcкι <nikkhahbamdad@gmail.com>
2016-06-22 13:02:08 -0400

    Update Version.lua

--- plugins/Version.lua
+++ plugins/Version.lua
@@ -2,7 +2,7 @@ do
 
 function run(msg, matches)
   local version = [[ *Version Text* ]]
-send_api_msg(msg, get_receiver_api(msg), version, true, 'md')
+send_api_msg(msg, get_receiver_api(msg), version, true, 'html')
 end
 return {
   description = "Shows Bot Version", 
