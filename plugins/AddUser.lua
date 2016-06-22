e7787555b747407a9665943c6f5d7f3db786d92f
Mя Jαcкι <nikkhahbamdad@gmail.com>
2016-06-22 17:33:01 -0400

    Create AddUser.lua

---
+++ plugins/AddUser.lua
@@ -0,0 +1,36 @@
+local function run(msg, matches)
+  
+  local signin = "*Your Have an Account!*"
+  local user = msg.from.id
+  checkuser = io.popen("less Text/Users/"..user.."/.check"):read('*all')
+  
+ if checkuser == "user="..msg.from.id then
+    send_api_msg(msg, get_receiver_api(msg), singin, true, 'md')
+    
+end
+
+local text = "user="..msg.from.id
+  local b = 1
+  while b ~= 0 do
+    text = text:trim()
+    text,b = text:gsub('^!+','')
+  end
+  local userf = msg.from.id
+  local adduser = "*Your Account Has Been Created!*"
+  local file = io.open("./Text/Users/"..userf.."/.check", "w")
+  file:write(text)
+  file:flush()
+  file:close()
+  send_api_msg(msg, get_receiver_api(msg), adduser, true, 'md')
+  
+    end
+ end 
+      
+return {
+  description = "Shows Bot Help Text", 
+  usage = "/help: Shows Bot Help Text",
+  patterns = {
+    "^/sign_up",
+  }, 
+  run = run 
+}
